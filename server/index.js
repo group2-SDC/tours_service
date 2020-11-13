638fb21eead86bbd870b33b417fcd4bbe60b55a1
Jordan Weiner <weinerj8@gmail.com>
2020-11-12 21:39:52 -0800

    Added request handler for retrieving top tours per category

--- server/index.js
+++ server/index.js
@@ -18,9 +18,24 @@ app.get('/api/listings/:id/tours/categories', (req, res) => {
 });
 
 app.get('/api/listings/:id/tours/categories/recommended', (req, res) => {
+  // missing logic for getting languages
   const listingId = req.params.id;
-  const sqlString = 'SELECT tours.id AS tours_id, tours.name AS tours_name, tours.* from tours INNER JOIN listings ON tours.listings_id = listings.id WHERE listings.id = ? ORDER BY tours.bookings DESC LIMIT 8';
+  const sqlString = 'SELECT tours.id AS tours_id, tours.name AS tours_name, tours.* FROM tours INNER JOIN listings ON tours.listings_id = listings.id WHERE listings.id = ? ORDER BY tours.bookings DESC LIMIT 8';
   db.query(sqlString, [listingId], (err, results) => {
+    if (err) {
+      res.sendStatus(404);
+    } else {
+      res.send(results);
+    }
+  });
+});
+
+app.get('/api/listings/:id/tours/categories/:categoryId', (req, res) => {
+  // missing logic for getting languages
+  const listingId = req.params.id;
+  const categoryId = req.params.categoryId;
+  const sqlString = 'SELECT tours.id AS tours_id, tours.name AS tours_name, tours.* FROM tours, listings, categories WHERE tours.listings_id = listings.id AND listings.id = ? AND categories.id = tours.categories_id AND categories.id = ? ORDER BY tours.bookings DESC LIMIT 8';
+  db.query(sqlString, [listingId, categoryId], (err, results) => {
     if (err) {
       res.sendStatus(404);
     } else {
