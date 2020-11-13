03b7be00c51bca5fe36a859f3e2ce493a74a429a
Jordan Weiner <weinerj8@gmail.com>
2020-11-12 22:06:57 -0800

    Updated all request handlers and added patch handler

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ app.use(express.static('public'));
 
 app.get('/api/listings/:id/tours/categories', (req, res) => {
   const listingId = req.params.id;
-  const sqlString = 'SELECT DISTINCT categories.id, categories.name, categories.description, tours.photo, tours.bookings FROM categories INNER JOIN tours ON categories.id = tours.categories_id INNER JOIN listings ON tours.listings_id = listings.id WHERE listings.id = ? ORDER BY tours.bookings DESC';
+  const sqlString = 'SELECT DISTINCT categories.id, categories.name, categories.description, tours.photo FROM categories, tours WHERE categories.id = tours.categories_id AND tours.listings_id = ?';
   db.query(sqlString, [listingId], (err, results) => {
     if (err) {
       res.sendStatus(404);
@@ -20,7 +20,7 @@ app.get('/api/listings/:id/tours/categories', (req, res) => {
 app.get('/api/listings/:id/tours/categories/recommended', (req, res) => {
   // missing logic for getting languages
   const listingId = req.params.id;
-  const sqlString = 'SELECT tours.id AS tours_id, tours.name AS tours_name, tours.* FROM tours INNER JOIN listings ON tours.listings_id = listings.id WHERE listings.id = ? ORDER BY tours.bookings DESC LIMIT 8';
+  const sqlString = 'SELECT tours.id AS tours_id, tours.name AS tours_name, tours.*, categories.name AS categories_name FROM tours, categories WHERE tours.listings_id = ? AND categories.id = tours.categories_id ORDER BY tours.bookings DESC LIMIT 8';
   db.query(sqlString, [listingId], (err, results) => {
     if (err) {
       res.sendStatus(404);
@@ -34,12 +34,25 @@ app.get('/api/listings/:id/tours/categories/:categoryId', (req, res) => {
   // missing logic for getting languages
   const listingId = req.params.id;
   const categoryId = req.params.categoryId;
-  const sqlString = 'SELECT tours.id AS tours_id, tours.name AS tours_name, tours.* FROM tours, listings, categories WHERE tours.listings_id = listings.id AND listings.id = ? AND categories.id = tours.categories_id AND categories.id = ? ORDER BY tours.bookings DESC LIMIT 8';
+  const sqlString = 'SELECT tours.id AS tours_id, tours.name AS tours_name, tours.*, categories.name AS categories_name FROM tours, categories WHERE tours.listings_id = ? AND categories.id = tours.categories_id AND categories.id = ? ORDER BY tours.bookings DESC LIMIT 8';
   db.query(sqlString, [listingId, categoryId], (err, results) => {
     if (err) {
       res.sendStatus(404);
     } else {
       res.send(results);
+    }
+  });
+});
+
+app.patch('/api/listings/:id/tours/:tourId/:status', (req, res) => {
+  const tourId = req.params.tourId;
+  const newStatus = !req.params.status;
+  const sqlString = 'UPDATE tours SET favorite = ? WHERE id = ?';
+  db.query(sqlString, [newStatus, tourId], (err, results) => {
+    if (err) {
+      res.sendStatus(400);
+    } else {
+      res.sendStatus(201);
     }
   });
 });
