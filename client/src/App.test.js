951891b2ffcaec3b9a525791563e8c444c50b994
Jordan Weiner <weinerj8@gmail.com>
2020-11-14 13:40:52 -0800

    Comment cleanup

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import { shallow, mount, render } from 'enzyme';
-// import Adapter from 'enzyme-adapter-react-16';
 import App from './App.jsx';
-
-// configure({ adapter: new Adapter() });
 
 describe('<App />', () => {
   it('renders a div', () => {
