25309771e69463e603897919f39b9ab12193fe79
Jordan Weiner <weinerj8@gmail.com>
2020-11-14 17:00:44 -0800

    Refactored App functions for greater separation of concerns/cleaner code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,36 +9,15 @@ class App extends React.Component {
       view: 0,
       tabs: []
     };
-    this.fetchCategories = this.fetchCategories.bind(this);
-    this.fetchRecommendedTours = this.fetchRecommendedTours.bind(this);
-    this.fetchToursByCategory = this.fetchToursByCategory.bind(this);
-    this.populateTabs = this.populateTabs.bind(this);
-    this.createRecommendedTab = this.createRecommendedTab.bind(this);
-    this.fillCategories = this.fillCategories.bind(this);
   }
 
   componentDidMount() {
+    // Make all get requests for this listing's tours/categories when the page is loaded, so we can store the relevant information to display, already sorted, in state, and then just switch which tours/categories are displayed depending on the tab user clicks
     this.populateTabs();
   }
 
   fetchRecommendedTours(listingId) {
     return axios.get(`/api/listings/${listingId}/tours/categories/recommended`);
-  }
-
-  createRecommendedTab() {
-    return new Promise((resolve, reject) => {
-      this.fetchRecommendedTours(this.props.listingId)
-        .then(recommendedTours => {
-          const recTab = {
-            name: 'Recommended',
-            description: 'Our most popular tours and activities',
-            displayNumber: 4,
-            tours: recommendedTours.data
-          }
-          resolve(recTab);
-        })
-        .catch(err => reject(err));
-    });
   }
 
   fetchCategories(listingId) {
@@ -49,44 +28,53 @@ class App extends React.Component {
     return axios.get(`/api/listings/${listingId}/tours/categories/${categoryId}`);
   }
 
-  fillCategories() {
+  createRecommendedTab() {
     return new Promise((resolve, reject) => {
-      this.fetchCategories(this.props.listingId)
-        .then(categories => {
-          const categoryPromises = categories.data.map(category => {
-            return new Promise((resolve, reject) => {
-              this.fetchToursByCategory(this.props.listingId, category.id)
-                .then(toursForCategory => {
-                  category.tours = toursForCategory.data,
-                  category.displayNumber = 4
-                  resolve(category)
-                })
-                .catch(err => reject(err));
-            })
-          });
-          return Promise.all(categoryPromises)
+      this.fetchRecommendedTours(this.props.listingId)
+        .then(recommendedTours => {
+          const recTab = {
+            name: 'Recommended',
+            description: 'Our most popular tours and activities',
+            displayMax: 4,
+            tours: recommendedTours.data
+          }
+          resolve(recTab);
         })
-        .then(filledCategories => resolve(filledCategories))
-        .catch(err => reject(err))
-    }) 
+        .catch(err => reject(err));
+    });
+  }
+
+  createCategoryTabs(categories) {
+      const categoryPromises = categories.map(category => {
+        return new Promise((resolve, reject) => {
+          this.fetchToursByCategory(this.props.listingId, category.id)
+            .then(toursForCategory => {
+              category.tours = toursForCategory.data;
+              category.displayMax = 4;
+              resolve(category);
+            })
+            .catch(err => reject(err));
+        })
+      });
+      return Promise.all(categoryPromises)
   }
 
   populateTabs() {
     const populatedTabs = [];
+    const browseTab = {
+      name: 'Browse',
+      displayMax: 6
+    };
     this.createRecommendedTab()
-      .then(recTab => {
-        populatedTabs.push(recTab);
+      .then(recTab => populatedTabs.push(recTab))
+      .then(() => this.fetchCategories(this.props.listingId))
+      .then(categories => {
+        const topCategories = categories.data.slice(0, 4);
+        browseTab.categories = categories.data.slice(4);
+        return this.createCategoryTabs(topCategories);
       })
-      .then(() => this.fillCategories())
-      .then(filledCategories => {
-        const categoryTabs = filledCategories.slice(0, 4);
-        const categoriesForBrowse = filledCategories.slice(4);
+      .then(categoryTabs => {
         categoryTabs.forEach(categoryTab => populatedTabs.push(categoryTab));
-        const browseTab = {
-          name: 'Browse',
-          displayNumber: 6,
-          categories: categoriesForBrowse
-        };
         populatedTabs.push(browseTab);
         this.setState({
           tabs: populatedTabs
