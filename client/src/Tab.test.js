bd4d6f5a16cba35eee19dc01892fc6c30f0f4288
Jordan Weiner <weinerj8@gmail.com>
2020-11-16 11:23:34 -0800

    Made some initial attempts at testing TabBar/Tab but running into questions/errors.

---
+++ client/src/Tab.test.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { shallow, mount, render } from 'enzyme';
+import Tab from './Tab.jsx';
+
+// simulate clicking on tab 
+// check App state.view and confirm it equals the correct tab
