2afc1407743a32dbe69b78dd9ce16184b485ce4b
Jordan Weiner <weinerj8@gmail.com>
2020-11-16 20:01:39 -0800

    Successfully displaying Quick View button on top of tour photos, working on hover functionality.

--- client/src/components/TourPreview.jsx
+++ client/src/components/TourPreview.jsx
@@ -10,9 +10,41 @@ const StyledTourPreview = styled.div`
   margin: 20px;
 `;
 
+const ImageContainer = styled.div`
+  background-image: url(${props => props.photo});
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+  width: 261px;
+  height: 173px;
+`;
+
+const ImageOverlay = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+`
+
+const QuickViewButton = styled.button`
+  background-color: white;
+  border: 1px solid black;
+  border-radius: 3px;
+  padding: 6px 10px;
+  font-weight: 500;
+  font-size: 12px;
+  width: 100px;
+`
+
 const TourPreview = (props) => (
   <StyledTourPreview>
-    <img src={props.item.photo + `?random=${props.item.id}`} width="225" height="170"/>
+    <ImageContainer photo={props.item.photo + `?random=${props.item.id}`}>
+      <ImageOverlay>
+        <QuickViewButton>Quick View</QuickViewButton>
+      </ImageOverlay>
+    </ImageContainer>
     {props.item.name} <br />
     {props.item.base_price} per adult
   </StyledTourPreview>
