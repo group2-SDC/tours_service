17c3153603799b91a6d92fb58db66dc1d1e60bff
Jordan Weiner <weinerj8@gmail.com>
2020-11-16 16:32:45 -0800

    Reorganized src folder to separate components from tests

--- client/index.js
+++ client/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './src/App.jsx';
+import App from './src/components/App.jsx';
 
 ReactDOM.render(<App listingId="1"/>, document.getElementById('app'));
 
