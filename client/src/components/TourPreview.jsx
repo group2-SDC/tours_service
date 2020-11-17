5e765a8adb1a2612b89a52b73b9640a2c8e200c4
Jordan Weiner <weinerj8@gmail.com>
2020-11-16 23:02:38 -0800

    Calling it a night on TourPreview MVP work.

--- client/src/components/TourPreview.jsx
+++ client/src/components/TourPreview.jsx
@@ -19,54 +19,12 @@ const TourName = styled.span`
   padding: 0 10px;
 `;
 
-const InfoContainer = styled.div`
-  display: flex;
-  justify-content: flex-start;
-  padding: 5px 10px;
-`
-
-const PriceContainer = styled.span`
-  display: flex;
-  flex-direction: column;
-  margin-right: 30px;
-  justify-content: flex-end;
-  text-align: left;
-`
-const Price = styled.span`
-  display: flex;
-  font-size: 12px;
-`
-
-const Bold = styled.span`
-  font-weight: 500;
-  margin-right: 5px;
-`
-
-const MoreInfoButton = styled.button`
-  border: 1px solid black;
-  border-radius: 18px;
-  background-color: black;
-  font-size: 14px;
-  color: white;
-  padding: 8px 16px;
-  width: 100px;
-  text-align: center;
-`;
-
 const TourPreview = (props) => (
     <StyledTourPreview onMouseOver={() => props.toggleHover(props.index)} onMouseLeave={props.toggleHover}>
       <TourPreviewImage isHovered={props.isHovered} photo={props.item.photo} id={props.item.id}/>
       <TourName>{props.item.name}</TourName>
       <TourPreviewReviews reviews={props.item.reviews} avgRating={props.item.avg_rating}/>
-      <InfoContainer>
-        <PriceContainer>
-          <Price>
-            <Bold>${props.item.base_price}</Bold>
-            <span>per adult</span>
-          </Price>
-        </PriceContainer>
-        <MoreInfoButton>More info</MoreInfoButton>
-      </InfoContainer>
+      <TourPreviewInfo price={props.item.base_price}/>
     </StyledTourPreview>
 );
 
