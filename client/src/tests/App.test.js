9b001e954e5b9fc476ac916966bf38a359439e20
Jordan Weiner <weinerj8@gmail.com>
2020-11-19 16:10:47 -0800

    Added tests re modal to App test suite

--- client/src/tests/App.test.js
+++ client/src/tests/App.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { shallow, mount, render } from 'enzyme';
 import App from '../components/App.jsx';
 import { sampleTabs } from './sampleData.js';
+import Modal from '../components/Modal.jsx';
 
 describe('<App />', () => {
   const wrapper = shallow(<App listingId="1"/>, { disableLifecycleMethods: true });
@@ -14,8 +15,43 @@ describe('<App />', () => {
   });
   it('updates showAll property of current view tab when showAllItems is invoked', () => {
     wrapper.instance().setState({tabs: sampleTabs});
-    wrapper.instance().showAllItems()
+    wrapper.instance().showAllItems();
     expect(wrapper.instance().state.tabs[wrapper.instance().state.view].showAll).toBe(true);
   });
+});
 
+describe('<App /> and modal rendering', () => {
+  const sampleTab = sampleTabs[0].items[0];
+  const wrapper = shallow(<App listingId="1"/>, { disableLifecycleMethods: true });
+  it('renders modal when toggleModal is invoked and state.showModal is initially false', () => {
+    wrapper.instance().setState({showModal: false});
+    wrapper.instance().toggleModal('e', sampleTab);
+    expect(wrapper.instance().state.showModal).toBe(true);
+    expect(wrapper.find(Modal)).toHaveLength(1);
+  });
+  it('updates currentItem to be the item passed into toggleModal when invoked, when state.showModal is initially false', () => {
+    wrapper.instance().setState({showModal: false});
+    wrapper.instance().toggleModal('e', sampleTab);
+    expect(wrapper.instance().state.currentItem).toEqual(sampleTab);
+  });
+  it('does not render modal when toggleModal is invoked and state.showModal is initially true', () => {
+    wrapper.instance().setState({showModal: true});
+    wrapper.instance().toggleModal('e');
+    expect(wrapper.instance().state.showModal).toBe(false);
+    expect(wrapper.find(Modal)).toHaveLength(0);
+  });
+  it('updates currentItem to be null when toggleModal is invoked and state.showModal is initially true', () => {
+    wrapper.instance().setState({showModal: true});
+    wrapper.instance().toggleModal('e');
+    expect(wrapper.instance().state.currentItem).toBeFalsy();
+  });
+  it('toggles modal when anywhere in the BodyContainer is clicked', () => {
+    wrapper.instance().setState({showModal: true});
+    wrapper.find('BodyContainer').simulate('click');
+    expect(wrapper.instance().state.showModal).toBe(false);
+  });
+  it('renders overlay when modal is active', () => {
+    wrapper.instance().setState({showModal: true});
+    expect(wrapper.find('Overlay')).toHaveLength(1);
+  });
 });
